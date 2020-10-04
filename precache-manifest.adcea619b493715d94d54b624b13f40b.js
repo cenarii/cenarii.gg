ce632c4992a7fe8c8b76bb86fbe93671214af65f
William Harvey <cenarii13@gmail.com>
2020-10-04 13:51:11 -0400

    Deployment from Master Branch

---
+++ precache-manifest.adcea619b493715d94d54b624b13f40b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1efaf9c1e5dcf4f1811600f0bc2561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8017b57e709db47934b7",
+    "url": "/static/css/main.0835241a.chunk.css"
+  },
+  {
+    "revision": "6b4e01cb50d1bed57a65",
+    "url": "/static/js/2.5f9a7c2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f9a7c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8017b57e709db47934b7",
+    "url": "/static/js/main.0381e4fb.chunk.js"
+  },
+  {
+    "revision": "cc713bcd266681ec171b",
+    "url": "/static/js/runtime-main.065f647a.js"
+  },
+  {
+    "revision": "29aa9f318624d2d9b83b900b1bb22d6c",
+    "url": "/static/media/matthewClassic.29aa9f31.jpg"
+  }
+]);
